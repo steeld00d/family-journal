2565848438d47d89c502c392572ac764308f2e04
Kevin Rice <kevin.j.rice@gmail.com>
2012-07-21 06:15:14 -0500

    Initial revision

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyJournal::Application.config.secret_token = '5025f9013a267feb9da914bac138b2b9a9b0ca5ec02dc9ff781f56145a9a6289d815364537bba0240c4e0e3685ccb15b85e8df968b79477e1e6a4679dc5d9be6'
